08a24816163f135899135b188da10ca37d33d454
i2c2-caj <i2c2.caj@gmail.com>
2014-03-11 22:00:13 -0600

    Finished refactoring deleteRow(), getRow(), and getValues(). Renamed getRowByID() to getValues().

--- db.py
+++ db.py
@@ -5,17 +5,22 @@ LAST UPDATE: 3/10/2014
 VERSION: 0.2.0
 
 DONE:
-== Constructors / Destructors==
+== Constructors / Destructors ==
 + DB.init
 
 == Modify Table (Setters) ==
 + DB.createTable
-+ DB.dropTable
 + DB.closeDB
++ DB.dropTable
++ DB.insertRow
+
 
 == Getters ==
 + DB.getColumnNames
 + DB.getConstraints
++ DB.deleteRow
++ DB.getRow
++ DB.getValues
 
 == Utilities ==
 + DB.dictToStrings
@@ -54,6 +59,8 @@ EVENTUALLY DO SOMETIME IN THE FAR FUTURE:
 import sqlite3 as sql
 
 class DB:
+    #__int__(self,
+    #        name) #Name of the DB to be created
     def __init__(self, name):
         #Load DB
         self.db = sql.connect(name)
@@ -77,14 +84,14 @@ class DB:
     def dropTable(self, table, force=False):
         answer = 'n'
         if (force == False):
-            self.warning('This will delete table "{0}" with all of it data'.format(table))
+            self.warning('This will delete table ({0}) with all of it data'.format(table))
             answer = raw_input('Are you sure you want to drop table "{0}"? (y/N)'.format(table))
-        if (answer.lower == 'y' or force == True):
+        if (answer.lower() == 'y' or force == True):
             #print '''DROP TABLE IF EXISTS {0}'''.format(table)
             self.cursor.execute('''DROP TABLE IF EXISTS {0}'''.format(table))
             self.db.commit()
         else:
-            print 'Disaster averted. Table "{0}" was not changed.'.format(table)
+            print 'Disaster averted. Table ({0}) was not changed.'.format(table)
         return
 
     #clearTable(self,
@@ -92,14 +99,15 @@ class DB:
     def clearTable(self, table, force=False):
         answer = 'n'
         if (force == False):
-            self.warning('This will delete all data from table "{0}"'.format(table))
-            answer = raw_input('Are you sure you want to delete all records from table "{0}"? (y/N)'.format(table))
+            self.warning('This will delete all data from table ({0})'.format(table))
+            answer = raw_input('Are you sure you want to delete all records from table ({0})? (y/N)'.format(table))
         if (answer.lower() == 'y' or force == True):
             self.cursor.execute('''DELETE FROM {0}'''.format(table))
             self.db.commit()
-            print 'Table "{0}" successfully cleared.'.format(table)
+            print 'Table ({0}) successfully cleared.'.format(table)
         else:
-            print 'Disaster averted. Table "{0}" was not changed.'.format(table)
+            print 'Disaster averted. Table ({0}) was not changed.'.format(table)
+        return
 
 ##    #alterTable(self,
 ##    #           table,   #Table
@@ -112,11 +120,11 @@ class DB:
 ##        return
 
     #insertRow(self,
-    #          row,  #ROW NAME
+    #          row,  #Row name
     #          info) #{key0:value0, ..., keyX:valueX}
     def insertRow(self, row, info):
         (keys, values) = self.paramDict(info)
-        print '''INSERT INTO {0} ({1}) VALUES ({2})'''.format(row, keys, values)
+        #print '''INSERT INTO {0} ({1}) VALUES ({2})'''.format(row, keys, values)
         try:
             #Will add row to DB unless there is a duplicate PRIMARY/UNIQUE key constraint.
             self.cursor.execute('''INSERT INTO {0} ({1}) VALUES ({2})'''.format(row, keys, values), info)
@@ -124,39 +132,45 @@ class DB:
             return
         except sql.IntegrityError:
             #Tell user to inherit and overwrite this method
-            print info
-            raise NotImplementedError('Could not insert row: You need to implement how you want to merge your ADT.')
+            constraints = self.getConstraints(row)
+            self.warning("The table ({0}) has the following constraints:".format(row))
+            self.warning("{0}".format(self.getConstraints(row)[0][1]))
+            self.warning("One of these constraints is being violated by insertRow() being called with duplicate values.")
+            raise NotImplementedError('Could not insert row: You need to implement how you want to merge your data.')
         
-##    #deleteRow(self,
-##    #          row,       #ROW NAME
-##    #          condition) #INSERT CONDITIONAL STATEMENT   
-##    def deleteRow(self, row, condition):
-##        #print '''DELETE FROM {0} WHERE {1}'''.format(row, condition)
-##        self.cursor.execute('''DELETE FROM {0} WHERE {1}'''.format(row, condition))
-##        self.db.commit()
-##        return
+    #deleteRow(self,
+    #          row,       #Row name
+    #          condition) #Condition to select data 
+    def deleteRow(self, row, condition):
+        print '''DELETE FROM {0} WHERE {1}'''.format(row, condition)
+        self.cursor.execute('''DELETE FROM {0} WHERE {1}'''.format(row, condition))
+        self.db.commit()
+        return
 
-##    #getRowByID(self,
-##    #           row,  #ROW NAME
-##    #           ID)   #ID OF ROW TO RETURN
-##    def getRowByID(self, row, ID):
-##        #print '''SELECT * FROM {0} WHERE ID={1}'''.format(row, ID)
-##        self.cursor.execute('''SELECT * FROM {0} WHERE ID=?'''.format(row), (ID))
-##        result = self.cursor.fetchone()
-##        if (result == None):
-##            print '##### No data from table "{0}" exists with ID "{1}" #####'.format(row, ID)
-##        return result
+    #getValues(self,
+    #          row,       #Row name
+    #          info,      #Dictionary of data involved in the query
+    #          condition) #Values to find
+    def getValues(self, row, info, condition):
+        (keys, values) = self.paramDict(info)
+        print '''SELECT ({0}) FROM {1} WHERE {2}'''.format(keys, row, condition)
+        self.cursor.execute('''SELECT {0} FROM {1} WHERE {2}'''.format(keys, row, condition), info)
+        result = self.cursor.fetchall()
+        if (result == []):
+            self.warning('No data from table ({0}) exists with statement ({1})'.format(row, condition))
+        return result
 
-##    #getRow(self,
-##    #           row,       #ROW NAME
-##    #           condition) #ID OF ROW TO RETURN
-##    def getRow(self, row, condition):
-##        print '''SELECT * FROM {0} WHERE {1}'''.format(row, condition)
-##        self.cursor.execute('''SELECT * FROM {0} WHERE {1}'''.format(row, condition))
-##        result = self.cursor.fetchone()
-##        if (result == None):
-##            print '##### No data from table "{0}" exists with statement "{1}" #####'.format(row, condition)
-##        return result
+    #getRow(self,
+    #       row,       #Row name
+    #       info)      #Dictionary of data involved in the query
+    def getRow(self, row, info):
+        (keys, values) = self.paramDict(info)
+        print '''SELECT * FROM {1} WHERE {0}={2}'''.format(keys, row, values)
+        self.cursor.execute('''SELECT * FROM {1} WHERE {0}={2}'''.format(keys, row, values), info)
+        result = self.cursor.fetchall()
+        if (result == []):
+            self.warning('No data from table ({0}) exists with statement ({1})'.format(row, str(keys)+'='+str(values)))
+        return result
 
 ##    #updateRow(self,
 ##    #          row,    #ROW NAME
@@ -228,6 +242,9 @@ class DB:
     #          info)    #Dictionary to parse
     """
     Parameterizes a dictionary into appropriate string values
+    Strings look like this:
+    key:    "key0, ..., keyX"
+    values: ":value0, ..., :valueX"
     """
     def paramDict(self, info):
         keys = ""
@@ -237,9 +254,6 @@ class DB:
             values += ":" + k + ", "
         keys = keys[:len(keys)-2]
         values = values[:len(values)-2]
-        #strings look like this
-        #   "key0, ..., keyX"
-        #   ":value0, ..., :valueX"
         return (keys, values)
 
     #getConstraints(self,
@@ -323,13 +337,63 @@ class DB:
         print '=== {0}! ==='.format(message)
         return
 
-    #safe(self) #Prevents SQL Injection attacks
+    #safe(self)
+    """
+    Prevents SQL Injection attacks
+    """
     def safe(self, string):
         safe_chars = 'abcdefghijklmonpqrstuvwxyzABCDEFGHIJKLMONPQRSTUVWXYZ0123456789@.-_+'
         for c in string:
             if not(c in safe_chars):
                 return 'False: {0}'.format(string)
         return 'True: {0}'.format(string)
+
+def Tests(db):
+    """
+    Helper Methods
+    """
+    print db.paramDict({'Name': 'Test', "Number": 9, "Count": 20})
+
+    """
+    DB Methods
+    """
+    #Drop table normally
+    #db.dropTable('MTG')
+    db.dropTable('MTG', 1)
+    #Force table to drop without asking
+    db.dropTable('Test', 1)
+    #Create table
+    db.createTable('MTG', '(name TEXT UNIQUE, color TEXT, count INTEGER CHECK(count > 0), ID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT)')
+    db.createTable('Test', '(test TEXT, name INTEGER PRIMARY KEY)')
+    #Insert rows
+    db.insertRow('MTG', {'name': 'Plain', 'color': 'WH', 'count': 10})
+    db.insertRow('MTG', {'name': 'Swamp', 'color': 'BK', 'count': 20})
+    db.insertRow('MTG', {'name': 'Mountain', 'color': 'RD', 'count': 30})
+    db.insertRow('MTG', {'name': 'Forest', 'color': 'G', 'count': 40})
+    db.insertRow('MTG', {'name': 'Island', 'color': 'BL', 'count': 50})
+    #Delete row
+    db.deleteRow('MTG', 'ID=1')
+    #Should crash: name is a unique field, cant have duplicate, 1
+    #db.insertRow('MTG', {'name': 'Swamp', 'color': 'BK', 'count': 50})
+    #Get row
+    print db.getRow('MTG', {'name': 'Plain'})
+    print db.getRow('MTG', {'color': 'G'})
+    #Get values
+    print db.getValues('MTG', {'name': 'Plain', 'ID':  1, 'color': 'WH', 'count': 50}, 'ID=:ID')
+    print db.getValues('MTG', {'name': 'Swamp', 'ID':  1, 'color': 'BK', 'count': 50}, 'color="BK"')
+    #Get Cconstraints
+    print db.getConstraints('MTG')
+    print db.getConstraints()
+    #Get column names
+    print db.getColumnNames('MTG')
+    #Print table
+    db.printTable('MTG')
+    #Clear table
+    db.clearTable('MTG')
+    #Clear table without asking
+    #db.clearTable('MTG', 1)
+    #Print table
+    db.printTable('MTG')
 
 def main():
     db = DB('MTG.sql')
@@ -347,26 +411,7 @@ def main():
 ##    hack = "x' AND members.email IS NULL; --"
 ##    print db.safe(hack)
 
-    db.dropTable('MTG', 1)
-    db.dropTable('Test', 1)
-
-    db.createTable('MTG', '(name TEXT UNIQUE, color TEXT, count INTEGER CHECK(count > 0), ID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT)')
-    db.insertRow('MTG', {'name': 'Plain', 'color': 'WH', 'count': 50})
-    db.insertRow('MTG', {'name': 'Swamp', 'color': 'BK', 'count': 50})
-    db.insertRow('MTG', {'name': 'Swamp', 'color': 'BK', 'count': 50})
-
-    #db.createTable('Test', '(test TEXT, name INTEGER PRIMARY KEY)')
-
-    #GET THIS TO WORK WITH MULTIPLE TABLES IN DATABASE
-
-    #print db.getConstraints('MTG')
-    #print db.getConstraints('Test')
-    #print db.getConstraints()
-
-    #print db.clearTable('MTG')
-    #print db.getColumnNames('MTG')
-
-    #db.printTable('MTG')
+    Tests(db)
     
     db.closeDB()
 
